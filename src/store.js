9e73485e3a6ccc12dfc2c300bc4b00297356e88b
Adrian <apgomez85@gmail.com>
2018-10-15 19:59:54 -0700

    added alert messages

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 //Reducers
-//ToDo
+import notifyReducer from './reducers/notifyReducer';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBtLL7BbGrq-15H01C3o0YwQsr3JgYywQs',
@@ -36,7 +36,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer // <- needed if using firestore
+  firestore: firestoreReducer,
+  notify: notifyReducer
 });
 
 //Create initial state
